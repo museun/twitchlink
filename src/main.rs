1971657b9f2dbed3f002f38a6486f90b11468f05
museun <museun@outlook.com>
2019-02-27 02:41:12 -0500

    major cleanups

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use getopts::Options;
 use serde::Serialize;
-use std::env;
 
 #[derive(Debug, Serialize, PartialEq, PartialOrd, Eq, Ord)]
 struct Stream {
@@ -8,7 +7,7 @@ struct Stream {
     bandwidth: String,
     link: String,
     #[serde(skip)]
-    quality: i32,
+    quality: Option<u32>,
     #[serde(rename = "type")]
     ty: String,
 }
@@ -18,76 +17,66 @@ impl Stream {
         let playlist = Self::playlist(channel)?;
 
         static VIDEO: &str = "VIDEO=";
-        static RESOLUTION: &str = "RESOLUTION=";
-        static BANDWIDTH: &str = "BANDWIDTH=";
 
         let mut map = std::collections::HashMap::new();
-
-        // TODO look at this. this shouldn't be done like this
-
-        let mut quality = String::new();
-        let mut resolution = String::new();
-        let mut bandwidth = String::new();
+        let (mut quality, mut resolution, mut bandwidth) =
+            (String::new(), String::new(), String::new());
 
         for line in playlist.lines() {
             if line.contains(VIDEO) {
-                // #EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=6874872,RESOLUTION=1920x1080,CODECS="avc1.4D402A,mp4a.40.2",VIDEO="chunked"
                 let (index, _) = line
                     .match_indices(VIDEO)
                     .next()
                     .ok_or_else(|| format!("cannot parse playlist for `{}`", channel))?;
 
-                let offset = index + VIDEO.len();
-                quality = line[offset..].replace("\"", "");
+                quality = line[index + VIDEO.len()..].replace("\"", "");
 
-                {
-                    let pos = line.find(BANDWIDTH).unwrap();
+                let search = |q: &str| {
+                    let pos = line.find(q).unwrap();
                     let end = (&line[pos..].find(',')).unwrap() + pos;
-                    bandwidth = line[pos + BANDWIDTH.len()..end].to_string();
-                }
+                    &line[pos + q.len()..end]
+                };
 
-                {
-                    let pos = line.find(RESOLUTION).unwrap();
-                    let end = (&line[pos..].find(',')).unwrap() + pos;
-                    resolution = line[pos + RESOLUTION.len()..end].to_string();
-                }
+                bandwidth = search("BANDWIDTH=").to_string();
+                resolution = search("RESOLUTION=").to_string();
             }
+
             if quality.is_empty() || line.starts_with('#') {
                 continue;
             }
-            if quality == "chunked" {
-                map.insert(
-                    9999,
-                    Stream {
-                        link: line.to_string(),
-                        resolution: resolution.to_string(),
-                        bandwidth: bandwidth.to_string(),
-                        quality: 9999,
-                        ty: "best".into(),
-                    },
-                ); // this is the source quality
-            } else if let Ok(q) = quality[..3].parse::<i32>() {
-                map.insert(
-                    q,
-                    Stream {
-                        link: line.to_string(),
-                        resolution: resolution.to_string(),
-                        bandwidth: bandwidth.to_string(),
-                        quality: q,
-                        ty: format!("{}p", q),
-                    },
-                );
-            }
 
-            // what is this doing?
-            // not sure if we can drain instead, this might be partial data
-            resolution.clear();
-            bandwidth.clear();
+            let s = match (quality.as_str(), quality[..3].parse::<u32>()) {
+                ("chunked", ..) => Stream {
+                    link: line.to_string(),
+                    resolution: resolution.drain(..).collect(),
+                    bandwidth: bandwidth.drain(..).collect(),
+                    quality: None,
+                    ty: "best".into(),
+                },
+                (.., Ok(n)) => Stream {
+                    link: line.to_string(),
+                    resolution: resolution.drain(..).collect(),
+                    bandwidth: bandwidth.drain(..).collect(),
+                    quality: Some(n),
+                    ty: format!("{}p", n),
+                },
+                (s, ..) => {
+                    eprintln!("unknown quality: {}", s);
+                    quality.clear();
+                    continue;
+                }
+            };
+
+            map.insert(s.quality, s);
             quality.clear();
         }
 
         let mut list = map.drain().map(|(_, v)| v).collect::<Vec<_>>();
-        list.sort_unstable_by(|a, b| b.quality.cmp(&a.quality));
+        list.sort_unstable_by(|a, b| match (a.quality, b.quality) {
+            (Some(a), Some(b)) => b.cmp(&a),
+            (None, ..) => std::cmp::Ordering::Less,
+            (.., None) => std::cmp::Ordering::Greater,
+        });
         Ok(list)
     }
 
@@ -98,7 +87,7 @@ impl Stream {
         ))
         .set(
             "Client-ID",
-            &env::var("TWITCH_CLIENT_ID").map_err(|_| "TWITCH_CLIENT_ID is not set")?,
+            &std::env::var("TWITCH_CLIENT_ID").map_err(|_| "TWITCH_CLIENT_ID is not set")?,
         )
         .call();
 
@@ -147,7 +136,7 @@ fn main() -> Result<(), String> {
     const PLAYER: &str = r#"C:\Program Files\DAUM\PotPlayer\PotPlayerMini64.exe"#;
 
     let (program, args) = {
-        let mut args = env::args();
+        let mut args = std::env::args();
         (args.next().unwrap(), args.collect::<Vec<_>>())
     };
 
@@ -184,7 +173,7 @@ fn main() -> Result<(), String> {
     }
 
     let stream = streams
-        .last()
+        .first()
         .ok_or_else(|| format!("stream `{}` is offline", channel))?;
 
     std::process::Command::new(player)
@@ -195,6 +184,6 @@ fn main() -> Result<(), String> {
 }
 
 fn print_usage(program: &str, opts: &Options) -> ! {
-    print!("{}", opts.usage(&format!("usage: {} stream", program)));
+    print!("{}", opts.usage(&format!("usage: {} <stream>", program)));
     std::process::exit(1);
 }
